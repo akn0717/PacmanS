0cc1512421f50a46fe933ac14281019fe70533ec
Khoa <anhkhoan@sfu.ca>
2023-07-18 12:08:43 -0700

    refactor menus into classes

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import argparse
-
+import pygame
+import global_configs
 from canvas import Canvas
+from menus.main_menu import Main_Menu
 
 
 def run(args):
-    canvas = Canvas(20)
-    canvas.populateCanvas()
-    canvas.draw()
-    return
+    print("Starting the game...")
+    pygame.init()
+    global_configs.SCREEN_WINDOW = pygame.display.set_mode()
+    (
+        global_configs.SCREEN_WIDTH,
+        global_configs.SCREEN_HEIGHT,
+    ) = global_configs.SCREEN_WINDOW.get_size()
+    pygame.display.set_caption("PacmanS")
+    menu = Main_Menu()
+    while True:
+        menu = menu.main()
+        if not (menu):
+            break
+
+    pygame.quit()
 
 
 if __name__ == "__main__":
---
+++ menus/menu.py
@@ -0,0 +1,9 @@
+class Menu:
+    def __init__(self):
+        pass
+
+    def update():
+        pass
+
+    def draw():
+        pass
