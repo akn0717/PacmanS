ff65533b72d23bdcd7da70671a138997dd2eea41
Khoa <anhkhoan@sfu.ca>
2023-07-19 00:44:48 -0700

    add game event buttons

--- game/gameplay_menu.py
+++ game/gameplay_menu.py
@@ -1,14 +1,32 @@
 from game.base_menu import Menu
 from game.canvas import Canvas
+import pygame
 
 
-class gameplay_menu(Menu):
+class Gameplay_Menu(Menu):
     def __init__(self, host: str = None) -> None:
         super().__init__()
-        if host is not None:  # is a client
+        if (
+            host is not None
+        ):  # is a client, TODO: recieve and init the game board based on the game board from host
             pass
         else:  # is a server, responsible for generating the game board
             self.canvas = Canvas()
 
     def main(self):
-        pass
+        isRunning = True
+        while isRunning:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    isRunning = True
+                elif event.type == pygame.K_UP:
+                    pass
+                elif event.type == pygame.K_DOWN:
+                    pass
+                elif event.type == pygame.K_LEFT:
+                    pass
+                elif event.type == pygame.K_RIGHT:
+                    pass
+
+            self.canvas.update()
+            self.canvas.draw()
