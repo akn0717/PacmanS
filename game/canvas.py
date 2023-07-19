01f112ba769ded68b8656fc11422f9eea76e3373
Khoa <anhkhoan@sfu.ca>
2023-07-19 00:45:04 -0700

    add load game assets

--- game/canvas.py
+++ game/canvas.py
@@ -1,17 +1,32 @@
 import numpy as np
 from game.global_variables import CANVAS_SIZE
+import pygame
+import game.global_variables as global_variables
 
 
 class Canvas:
-    def __init__(self, data: np.ndarray = None, shape=CANVAS_SIZE, auto_populate=False):
-        self.__data = np.ones(shape, dtype=np.int32)
+    def __init__(self, data: np.ndarray = None, shape=CANVAS_SIZE, auto_populate=True):
+        self.__data = None
         if data != None:
             self.__data = data.copy()
+        else:
+            self.__data = np.ones(shape, dtype=np.int32)
+            if auto_populate:
+                self.__dd = np.zeros_like(self.data)
+                self.populateCanvas()
+
         self.shape = data.shape
 
-        if auto_populate:
-            self.__dd = np.zeros_like(self.data)
-            self.populateCanvas()
+        self.asset_empty_block = pygame.image.load("../assets/EmptyBlock.png")
+        self.asset_block = pygame.image.load("../assets/Block.png")
+        num_players = 4
+        # TODO get player ids from the server and assign them with each image color
+        self.asset_players = [
+            pygame.image.load("../assets/PacmanYellow.png")
+            .convert_alpha()
+            .set_colorkey(((255, 255, 255) / num_players) * i)
+            for i in range(num_players)
+        ]
 
     @classmethod
     def fromData(cls, data: np.ndarray):
@@ -65,8 +80,14 @@ class Canvas:
         self.__dfsPopulation(i, j - 1)
         self.__dfsPopulation(i, j + 1)
 
-    def draw(self):
-        for i in range(self.size):
-            for j in range(self.size):
-                print(str(self.data[i, j]) + " ", end="")
-            print()
+    def draw(
+        self,
+    ):
+        # for i in range(self.size):
+        #     for j in range(self.size):
+        #         print(str(self.data[i, j]) + " ", end="")
+        #     print()
+        assert isinstance(global_variables.SCREEN_WINDOW, pygame.Surface)
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                global_variables.SCREEN_WINDOW.blit()
