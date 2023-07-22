3e5518dd48510aef107082e40049e9cc8d367861
Khoa <anhkhoan@sfu.ca>
2023-07-22 16:40:46 -0700

    add player object

---
+++ game/game_sprites.py
@@ -0,0 +1,14 @@
+import game.global_constants as global_constants
+
+
+class Pacman(Sprite):
+    def __init__(self, id, position):
+        self.id = id
+        self.position = position
+        self.score = 0
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
--- game/global_constants.py
+++ game/global_constants.py
@@ -1,1 +1,15 @@
+import pygame
+
 CANVAS_SIZE = (10, 10)
+IMAGE_ASSET_EMPTY_BLOCK = pygame.image.load("../assets/EmptyBlock.png")
+IMAGE_ASSET_WALL_BLOCK = pygame.image.load("../assets/Block.png")
+IMAGE_ASSET_DOT = pygame.image.load("")
+
+num_players = 4
+# TODO get player ids from the server and assign them with each image color
+IMAGE_ASSET_PLAYERS = [
+    pygame.image.load("../assets/PacmanYellow.png")
+    .convert_alpha()
+    .set_colorkey(((255, 255, 255) / num_players) * i)
+    for i in range(num_players)
+]
