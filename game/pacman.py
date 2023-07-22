5423e3c033c2012b63f455570e8cff8a28030b8f
Khoa <anhkhoan@sfu.ca>
2023-07-22 16:23:12 -0700

    add pacman object

---
+++ game/pacman.py
@@ -0,0 +1,11 @@
+class Pacman:
+    def __init__(self, id):
+        self.id = id
+        self.score = 0
+        self.position = (0, 0)
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
