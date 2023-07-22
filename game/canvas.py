61cabd9c4e4867f095d4c39e9cd4ae434f7c2d3c
Khoa <anhkhoan@sfu.ca>
2023-07-22 14:47:13 -0700

    move host manager to GameServer

--- game/canvas.py
+++ game/canvas.py
@@ -5,17 +5,11 @@ import game.global_variables as global_variables
 
 
 class Canvas:
-    def __init__(self, data: np.ndarray = None, shape=CANVAS_SIZE, auto_populate=True):
-        self.__data = None
-        if data != None:
-            self.__data = data.copy()
-        else:
-            self.__data = np.ones(shape, dtype=np.int32)
-            if auto_populate:
-                self.__dd = np.zeros_like(self.data)
-                self.populateCanvas()
+    def __init__(self, board_data):
+        self.__board_data = board_data
 
-        self.shape = data.shape
+        self.players = []  # player positions
+        self.__score_data = np.zeros_like(self.__board_data)
 
         self.asset_empty_block = pygame.image.load("../assets/EmptyBlock.png")
         self.asset_block = pygame.image.load("../assets/Block.png")
@@ -27,14 +21,6 @@ class Canvas:
             .set_colorkey(((255, 255, 255) / num_players) * i)
             for i in range(num_players)
         ]
-
-    @classmethod
-    def fromData(cls, data: np.ndarray):
-        cls(data=data)
-
-    @classmethod
-    def fromSize(cls, shape: int):
-        cls(shape=shape)
 
     def __blockType(self, i, j):
         code = 0
@@ -48,46 +34,31 @@ class Canvas:
             code |= (self.data[i][j + 1] == 1) << 3
         return code
 
-    def __dfsPopulation(self, i, j):
-        if (
-            i <= 0
-            or i >= self.size - 1
-            or j <= 0
-            or j >= self.size - 1
-            or self.__dd[i, j] == 1
-        ):
-            return 1
-        self.__dd[i, j] = 1
-
-        c = 0.8  # how many walls expected
-        self.data[i, j] = np.random.choice(np.array([0, 1]), p=[c, 1 - c])
-        if self.data[i, j] == 0:
-            self.__dfsPopulation(i - 1, j)
-            self.__dfsPopulation(i + 1, j)
-            self.__dfsPopulation(i, j - 1)
-            self.__dfsPopulation(i, j + 1)
-
-    def populateCanvas(self):
-        # set the border lines
-        for i in range(self.size):
-            self.__data[0, i] = self.data[i, 0] = 1
-            self.__data[i, self.size - 1] = self.data[self.size - 1, i] = 1
-
-        i, j = np.random.randint(1, self.size - 2), np.random.randint(1, self.size - 2)
-        self.__data[i, j] = 0
-        self.__dfsPopulation(i - 1, j)
-        self.__dfsPopulation(i + 1, j)
-        self.__dfsPopulation(i, j - 1)
-        self.__dfsPopulation(i, j + 1)
-
     def draw(
         self,
     ):
+        # Debug board
         # for i in range(self.size):
         #     for j in range(self.size):
         #         print(str(self.data[i, j]) + " ", end="")
         #     print()
+
         assert isinstance(global_variables.SCREEN_WINDOW, pygame.Surface)
         for i in range(self.shape[0]):
             for j in range(self.shape[1]):
-                global_variables.SCREEN_WINDOW.blit()
+                if self.__board_data[i][j] == 0 or self.__board_data[i][j] == 2:
+                    global_variables.SCREEN_WINDOW.blit(
+                        self.asset_empty_block,
+                        (i, j) * self.asset_empty_block.get_size[0],
+                    )
+                elif self.__board_data[i][j] == 1:
+                    global_variables.SCREEN_WINDOW.blit(
+                        self.asset_block,
+                        (i, j) * self.asset_block.get_size[0],
+                    )
+
+                if self.__board_data[i][j] == 2:
+                    pass  # TODO: Load dot image and blit it here
+
+        for i in range(len(self.players)):
+            global_variables.SCREEN_WINDOW.blit(self.asset_players[i], self.players[i])
---
+++ network/GameServer.py
@@ -0,0 +1,94 @@
+# adapted from https://realpython.com/python-sockets/
+import socket
+import threading
+import numpy as np
+import game.global_constants as global_constants
+
+
+class GameServer:
+    def __init__(self, port):
+        self.port = port
+        # socket.SOCK_STREAM is TCP
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # start server binding at specific port in TCP
+        self.socket.bind(("", self.port))
+        print("Server Started")
+        print("-----------------")
+        self.socket.listen()  # start listening for connection request
+        self.connections = []
+
+    def threadedConnection(self, conn):
+        while True:
+            recv_data = conn.recv(128)
+            if recv_data:
+                print("Message received from {}".format(conn))
+                print("Message is {}".format(recv_data.decode("utf-8")))
+            for connection in self.connections:
+                if connection != conn:
+                    connection.sendall(recv_data)
+        conn.close()
+        print("Connection closed.")
+
+    def __dfsPopulation(self, i, j):
+        if (
+            i <= 0
+            or i >= self.size - 1
+            or j <= 0
+            or j >= self.size - 1
+            or self.__dd[i, j] == 1
+        ):
+            return 1
+        self.__dd[i, j] = 1
+
+        c = 0.8  # how many walls expected
+        self.board_data[i, j] = np.random.choice(np.array([0, 1]), p=[c, 1 - c])
+        if self.data[i, j] == 0:
+            self.__dfsPopulation(i - 1, j)
+            self.__dfsPopulation(i + 1, j)
+            self.__dfsPopulation(i, j - 1)
+            self.__dfsPopulation(i, j + 1)
+
+    def populateCanvas(self):
+        # set the border lines
+        for i in range(self.size):
+            self.board_data[0, i] = self.data[i, 0] = 1
+            self.board_data[i, self.size - 1] = self.data[self.size - 1, i] = 1
+
+        i, j = np.random.randint(1, self.size - 2), np.random.randint(1, self.size - 2)
+        self.board_data[i, j] = 0
+        self.__dfsPopulation(i - 1, j)
+        self.__dfsPopulation(i + 1, j)
+        self.__dfsPopulation(i, j - 1)
+        self.__dfsPopulation(i, j + 1)
+
+    def populatePlayerPosition(self, num_players):
+        (n, m) = self.board_data.shape
+        potential_positions = []
+        for i in range(n):
+            for j in range(m):
+                if self.board_data[i, j] == 0:
+                    potential_positions.append((i, j))
+        return np.random.choice(potential_positions, num_players)
+
+    def initializeGameData(self):
+        self.board_data = np.ones(shape=global_constants.CANVAS_SIZE)
+        self.__dd = np.zeros_like(self.board_data)
+        self.populateCanvas()
+
+        self.players = self.populatePlayerPosition(4)
+
+    def startServer(self):
+        while True:
+            conn, _ = self.socket.accept()
+            self.connections.append(conn)
+            thread = threading.Thread(target=self.threadedConnection, args=(conn,))
+            thread.start()
+
+    def closeSocket(self):
+        self.socket.close()
+        print("Closed socket.")
+
+
+if __name__ == "__main__":
+    server = GameServer(5555)
+    server.startServer()
