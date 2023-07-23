18a2ee20ec786f2e90cf3a080789bc6f07978277
Chase Shen <chaseshen0626@gmail.com>
2023-07-22 19:05:00 -0700

    format code and comments

--- score_display.py
+++ score_display.py
@@ -28,19 +28,23 @@ icon_pacman_size = 50
 
 pacman_red_path = "assets/PacmanRed.svg"
 icon_pacman_red = pygame.image.load(pacman_red_path)
-icon_pacman_red = pygame.transform.scale(icon_pacman_red, (icon_pacman_size, icon_pacman_size))
+icon_pacman_red = pygame.transform.scale(icon_pacman_red, 
+                                         (icon_pacman_size, icon_pacman_size))
 
 pacman_green_path = "assets/PacmanGreen.svg"
 icon_pacman_green = pygame.image.load(pacman_green_path)
-icon_pacman_green = pygame.transform.scale(icon_pacman_green, (icon_pacman_size, icon_pacman_size))
+icon_pacman_green = pygame.transform.scale(icon_pacman_green, 
+                                           (icon_pacman_size, icon_pacman_size))
 
 pacman_blue_path = "assets/PacmanBlue.svg"
 icon_pacman_blue = pygame.image.load(pacman_blue_path)
-icon_pacman_blue = pygame.transform.scale(icon_pacman_blue, (icon_pacman_size, icon_pacman_size))
+icon_pacman_blue = pygame.transform.scale(icon_pacman_blue, 
+                                          (icon_pacman_size, icon_pacman_size))
 
 pacman_yellow_path = "assets/PacmanYellow.svg"
 icon_pacman_yellow = pygame.image.load(pacman_yellow_path)
-icon_pacman_yellow = pygame.transform.scale(icon_pacman_yellow, (icon_pacman_size, icon_pacman_size))
+icon_pacman_yellow = pygame.transform.scale(icon_pacman_yellow, 
+                                            (icon_pacman_size, icon_pacman_size))
 
 
 
@@ -56,13 +60,14 @@ while running:
 
     for i in range(4):
         
-        if i == 0:          # player 1 red
+        if i == 0:          # player 1 red top left
             score_text = font.render(f"{players[i]}: {scores[i]}", True, colors[i])
             text_width = score_text.get_width()
             text_height = score_text.get_height()
             text_x = margin
             text_y = margin
             window.blit(score_text, (text_x, text_y))
+
             icon_x = text_x + text_width + space
             icon_y = text_y - text_height / 2
             window.blit(icon_pacman_red, (icon_x, icon_y))
@@ -72,13 +77,14 @@ while running:
                 window.blit(icon_crown, (icon_x, icon_y - icon_pacman_size / 4 * 3))
             i += 1
 
-        elif i == 1:        # player 2 green
+        elif i == 1:        # player 2 green top right
             score_text = font.render(f"{players[i]}: {scores[i]}", True, colors[i])
             text_width = score_text.get_width()
             text_height = score_text.get_height()
             icon_x = window_width - margin - text_width - space - icon_pacman_size
             # icon_y = text_y - text_height / 2
             window.blit(icon_pacman_green, (icon_x, icon_y))
+
             text_x = icon_x + icon_pacman_size + space
             text_y = icon_y + icon_pacman_size / 2 - text_height / 2
             window.blit(score_text, (text_x, text_y))
@@ -88,13 +94,14 @@ while running:
                 window.blit(icon_crown, (icon_x, icon_y - icon_pacman_size / 4 * 3))
             i += 1
 
-        elif i == 2:        # player 3 blue   
+        elif i == 2:        # player 3 blue bottom left
             score_text = font.render(f"{players[i]}: {scores[i]}", True, colors[i])
             text_width = score_text.get_width()
             text_height = score_text.get_height()
             text_x = margin
             text_y = window_height - margin - text_height
             window.blit(score_text, (text_x, text_y))
+
             icon_x = text_x + text_width + space
             icon_y = text_y - text_height / 2
             window.blit(icon_pacman_blue, (icon_x, icon_y))
@@ -104,13 +111,14 @@ while running:
                 window.blit(icon_crown, (icon_x, icon_y - icon_pacman_size / 4 * 3))
             i += 1   
 
-        else:               # player 4 yellow
+        else:               # player 4 yellow bottom right
             score_text = font.render(f"{players[i]}: {scores[i]}", True, colors[i])
             text_width = score_text.get_width()
             text_height = score_text.get_height()
             icon_x = window_width - margin - text_width - space - icon_pacman_size
             # icon_y = text_y - text_height / 2
             window.blit(icon_pacman_yellow, (icon_x, icon_y))
+
             text_x = icon_x + icon_pacman_size + space
             text_y = icon_y + icon_pacman_size / 2 - text_height / 2
             window.blit(score_text, (text_x, text_y))
@@ -121,9 +129,11 @@ while running:
             i += 1
         
 
-        
-    # for testing
-    scores = [random.randint(0, 1000), random.randint(0, 1000), random.randint(0, 1000), random.randint(0, 1000)]
+    # random each score for testing
+    scores = [random.randint(0, 1000), 
+              random.randint(0, 1000), 
+              random.randint(0, 1000), 
+              random.randint(0, 1000)]
 
     pygame.display.flip()
     clock.tick(0.5)
