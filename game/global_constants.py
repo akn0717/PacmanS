70f4a24cb025bc172d3f55926f039f552ccab87d
Khoa <anhkhoan@sfu.ca>
2023-07-22 19:48:23 -0700

    refactor

--- game/global_constants.py
+++ game/global_constants.py
@@ -4,9 +4,9 @@ CANVAS_SIZE = (20, 20)
 PRIMARY_COLOR = (100, 100, 100)
 BLOCK_SIZE = (100, 100)
 MOVE_DIRECTIONS = [
-    np.asarray((0, 1)),
-    np.asarray((0, -1)),
-    np.asarray((1, 0)),
-    np.asarray((-1, 0)),
+    (0, 1),
+    (0, -1),
+    (1, 0),
+    (-1, 0),
 ]
 NUM_PLAYERS = 4
