6c8caeeb1377c8824f99af4ba5e2dd2a1269a014
Khoa <anhkhoan@sfu.ca>
2023-07-30 23:37:38 -0700

    change buffer schema

--- network/utils.py
+++ network/utils.py
@@ -1,16 +1,21 @@
 def concatBuffer(token, args=None):
     if args is not None:
-        return (str(token) + " " + " ".join(args)).encode()
+        return (str(token) + " " + " ".join(args) + "\n").encode()
     else:
-        return (str(token)).encode()
+        return (str(token) + "\n").encode()
 
 
-def splitBuffer(buffer: bytearray):
-    message = buffer.decode().split(" ")
-    message = list(filter(lambda x: x != "", message))
-    return message
+def splitBuffer(stringBuffer: str):
+    messages = stringBuffer.split("\n")
+    remainder = ""
+    if stringBuffer[-1] != "\n":
+        remainder = messages[-1]
+
+    messages.pop(-1)
+    return messages, remainder
 
 
-def flush(socket):  # fake flush
-    socket.sendall(" ".encode())
-    return
+def parseMessage(message):
+    data = message.split(" ")
+    # data = list(filter(lambda x: x != "", data))
+    return data
